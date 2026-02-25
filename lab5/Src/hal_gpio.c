9258bc761f7098d637cc7b12e0e0eace40e003c8
ConnorStevens <conngstevens@gmail.com>
2026-02-25 11:28:08 -0700

    Lab5 started, USART setup, I2C setup but not working

---
+++ lab5/Src/hal_gpio.c
@@ -0,0 +1,164 @@
+#include "stm32f072xb.h"
+#include <stdint.h>
+#include <stm32f0xx_hal.h>
+#include <stm32f0xx_hal_gpio.h>
+#include <hal_gpio.h>
+
+/**
+ * General GPIO Init function
+ */
+void My_HAL_GPIOx_Init(GPIO_TypeDef *GPIOx, GPIO_InitTypeDef *GPIO_Init)
+{
+    for (uint32_t pinNum = 0; pinNum < 16; pinNum++) {
+        //checks if current pinNum is user passed Pin to modify
+        if (GPIO_Init->Pin & (1U << pinNum)) {
+
+            // Setting MODER Reg
+            GPIOx->MODER &= ~(3U << (pinNum * 2)); //clears pin mode
+            GPIOx->MODER |= (GPIO_Init->Mode << (pinNum * 2)); //sets pin to passed Mode
+
+           //Setting OTYPER Reg (check for both regular output and alternate function push-pull)
+            if ((GPIO_Init->Mode == GPIO_MODE_OUTPUT_PP) || (GPIO_Init->Mode == GPIO_MODE_AF_PP)) {
+                GPIOx->OTYPER &= ~(1U << pinNum); // Push-pull
+            } else {
+                GPIOx->OTYPER |= (1U << pinNum); // Open-drain
+            }
+
+            //setting PUPDR reg
+            GPIOx->PUPDR &= ~(3U << (pinNum * 2)); //clears pin mode
+            GPIOx->PUPDR |= (GPIO_Init->Pull << (pinNum * 2)); //sets pin to passed pull-up or pull-down mode
+
+            //Setting OSPEEDR Reg
+            GPIOx->OSPEEDR &= ~(3U << (pinNum * 2)); // Reset pin reg bits
+            GPIOx->OSPEEDR |= (GPIO_Init->Speed << (pinNum * 2)); //Set pin reg bits to passed speed
+
+            uint32_t AFRreg;
+            uint32_t AFRPin = pinNum;
+            //Setting Alternate Function
+            if (pinNum >= 8) { // Checking what reg (high or low) to modify
+                AFRreg = 1;
+                AFRPin -= 8;
+            } else {AFRreg = 0;}
+            GPIOx->AFR[AFRreg] &= ~(0xF << (AFRPin * 4)); // Clear 4 reg bits
+            GPIOx->AFR[AFRreg] |= (GPIO_Init->Alternate << (AFRPin * 4)); // Set alternate bits in proper reg
+        }
+    }
+}
+
+void HAL_RCC_GPIOC_CLK_ENABLE(void)
+{
+    RCC->AHBENR |= (1 << 19); // Enable GPIOC clock bit (bit 19)
+}
+
+void HAL_RCC_GPIOB_CLK_ENABLE(void)
+{
+    RCC->AHBENR |= (1 << 18); // Enable GPIOB clock bit (bit 18)
+}
+
+void HAL_RCC_GPIOA_CLK_ENABLE(void)
+{
+    RCC->AHBENR |= (1 << 17); // Enable GPIOA clock bit (bit 17)
+}
+
+void RCC_TIM23_CLK_Enable(void) 
+{
+    RCC->APB1ENR |= 0x1; // Sets bits 1:0 (TIM2 and TIM3)
+    RCC->APB1ENR |= (1 << 1); // Sets bits 1:0 (TIM2 and TIM3)
+}
+
+void RCC_USART3_CLK_ENABLE(void)
+{
+    RCC->APB1ENR |= (1 << 18); // Sets bit 18 (USART3) to 1
+}
+
+
+void EXTI_Setup(EXTI_TypeDef *EXTI0, SYSCFG_TypeDef *EXTICR) {
+    EXTI0->IMR |= 0x1; //unmask interupt generation for line0
+    EXTI0->RTSR |= 0x1; //enable rising trigger detection for line0
+    SYSCFG->EXTICR[0] |= ~(0xF); //set multiplexer of EXTI0 to PA0
+}
+
+void USART_Setup(USART_TypeDef *USARTx, uint32_t baudRate) {
+  USARTx->CR1 &= ~(1 << 15); //Set OVER8 = 0
+  USARTx->CR1 |= (0x3 << 2); // Set bits 2 & 3 (TX and RX enable)
+  USARTx->CR1 |= (1 << 5); // set bit 5 (receive register not empty interupt)
+
+  uint32_t clk = HAL_RCC_GetHCLKFreq();
+  USARTx->BRR = clk / baudRate; // Set Baud rate to ~115,200 (divide 8MHz by 69)
+
+  USARTx->CR1 |= 0x1; //enable USART3
+}
+
+void I2Cx_Setup(I2C_TypeDef *I2Cx) {
+    I2Cx->TIMINGR = (0x1 << 28) // PRESC
+                  | (0x4 << 20) // SCLDEL
+                  | (0x2 << 16) // SCADEL
+                  | (0x0F << 8) // SCLH
+                  | (0x13);     // SCLL
+}
+
+void Init_I2C_Transaction(I2C_TypeDef *I2Cx, int RW, uint8_t Addr, uint8_t NumBytes) {
+    I2Cx->CR2 &= ~((0x7f << 16) | (0x3FF) | (1 << 25)); //clear NBYTES, SADD field, and Reset AUTOEND
+    I2Cx->CR2 |= ((Addr & 0x7F) << 1); // Set SADD (7-bit address goes in bits 7:1)
+    I2Cx->CR2 |= ((NumBytes & 0xFF) << 16); // Set NBYTES (8 bit value)
+    I2Cx->CR2 |= ((RW & 0x1) << 10); // Set read/write bit (Write = 0, Read = 1)
+    I2C2->CR2 |= (1 << 13); // Set START bit
+}
+/*
+void My_HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
+{
+}
+*/
+
+GPIO_PinState My_HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+{
+    if (GPIOx->IDR & GPIO_Pin) {
+        return GPIO_PIN_SET; // IDR reg is 1 (pin is high)
+    } else {
+        return GPIO_PIN_RESET; // IDR reg is 0 (pin is low)
+    }
+}
+
+
+void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
+{
+    if (PinState == GPIO_PIN_SET) {
+        GPIOx->BSRR |= GPIO_Pin; // Set the ODR reg for the passed pin
+    } else {
+        GPIOx->BSRR &= ~GPIO_Pin; // Reset the ODR reg for the passed pin
+    }
+}
+
+
+void My_HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+{
+    GPIOx->ODR ^= GPIO_Pin; // Toggle the output data register for the passed pin(s)
+}
+
+void TIM2_Setup(TIM_TypeDef *TIMx) {
+    TIMx->PSC = 0x1F3F; // set PSC to 7,999 (make clk 1KHz)
+    TIMx->ARR = 0xFA; // set ARR to 250
+    TIMx->DIER |= 0x1; //enable UEV interrupt
+    TIMx->CR1 |= 0x1; //enable time clk
+}
+
+void TIM3_Setup(TIM_TypeDef *TIMx) {
+    TIMx->PSC = 0x63;
+    TIMx->ARR = 0x64;
+    
+    // Clear and set channel modes properly
+    TIMx->CCMR1 &= ~(0x3 | (0x3 << 8));     // Clear CC1S and CC2S (output mode)
+    TIMx->CCMR1 &= ~(0x7 << 4);             // Clear OC1M
+    TIMx->CCMR1 |= (0x7 << 4);              // Set OC1M to 111 (PWM Mode 2)
+    TIMx->CCMR1 &= ~(0x7 << 12);            // Clear OC2M
+    TIMx->CCMR1 |= (0x6 << 12);             // Set OC2M to 110 (PWM Mode 1)
+    TIMx->CCMR1 |= (1 << 3) | (1 << 11);    // Enable preload for both channels
+    
+    TIMx->CCER |= (0x1) | (1 << 4);         // Enable output for channels 1 and 2
+    
+    TIMx->CCR1 = 20;  // 20% duty cycle
+    TIMx->CCR2 = 80;  // 20% duty cycle
+    
+    TIMx->CR1 |= 0x1;         // Enable timer
+}
+
--- lab5/Src/lab5.c
+++ lab5/Src/lab5.c
@@ -1,7 +1,30 @@
 #include "main.h"
+#include "stm32f072xb.h"
 #include "stm32f0xx_hal.h"
+#include "stm32f0xx_hal_gpio.h"
+#include <assert.h>
+#include <stdio.h>
+#include "hal_gpio.h"
+#include "core_cm0.h"
+#include "stm32f0xx_hal_gpio_ex.h"
+#include "stm32f0xx_hal_rcc.h"
 
 void SystemClock_Config(void);
+volatile uint8_t receiveReg;
+volatile uint8_t newData = 0;
+void sendChar(char c);
+void sendString(const char *str);
+
+typedef enum {
+    I2C_PASS = 0,
+    I2C_NACK = 1
+} I2C_Status;
+
+I2C_Status writeOp(uint8_t Byte);
+I2C_Status readOp(uint8_t *Byte);
+void NACKF_Error(void);
+
+
 
 /**
   * @brief  The application entry point.
@@ -13,12 +36,139 @@ int main(void)
   HAL_Init();
   /* Configure the system clock */
   SystemClock_Config();
+  HAL_RCC_GPIOB_CLK_ENABLE();
+  HAL_RCC_GPIOC_CLK_ENABLE();
+  RCC->APB1ENR |= (1 << 22); // Enable I2C2 CLK
+  RCC_USART3_CLK_ENABLE();
+
+  // Setup I2C SDA
+  GPIO_InitTypeDef iniStr = {GPIO_PIN_11,
+                            GPIO_MODE_AF_OD,
+                            GPIO_NOPULL,
+                          GPIO_SPEED_FREQ_LOW,
+                      GPIO_AF1_I2C2};
+
+  GPIO_InitTypeDef iniStr3 = {GPIO_PIN_4 | GPIO_PIN_5,
+                        GPIO_MODE_AF_PP,
+                        GPIO_NOPULL,
+                        GPIO_SPEED_FREQ_LOW,
+                        GPIO_AF1_USART3};
+  //Setup ALL LEDs and PC0
+  GPIO_InitTypeDef iniStr2 = {GPIO_PIN_8 | GPIO_PIN_7 |GPIO_PIN_6 | GPIO_PIN_9 |GPIO_PIN_0,
+                          GPIO_MODE_OUTPUT_PP,
+                          GPIO_NOPULL,
+                          GPIO_SPEED_FREQ_LOW};
+
+  My_HAL_GPIOx_Init(GPIOB, &iniStr);
+  iniStr.Pin = GPIO_PIN_13; // set to PB13
+  iniStr.Alternate = GPIO_AF5_I2C2; //set AF to 5
+  My_HAL_GPIOx_Init(GPIOB, &iniStr);
+  My_HAL_GPIOx_Init(GPIOC, &iniStr2);
+  iniStr2.Pin = GPIO_PIN_14; // Set inistr3 to only pin 14 for PB14 init
+  My_HAL_GPIOx_Init(GPIOB, &iniStr2);
+  My_HAL_GPIOx_Init(GPIOC, &iniStr3); //set PC4 and PC5
+  USART_Setup(USART3, 115200);
+
+  GPIOB->ODR |= (1 << 14); // Set PB14
+  GPIOC->ODR |= 0x1; //Set PC0
+
+  assert(((GPIOB->MODER >> (11*2)) & 0x3) == 0x2); //assert PB11 is in alternate mode (10)
+  assert(((GPIOB->MODER >> (13*2)) & 0x3) == 0x2); //assert PB13 is in alternate mode (10)
+  assert(((GPIOB->MODER >> (14*2)) & 0x3) == 0x1); //assert PB13 is in output mode (01)
+  assert(((GPIOC->MODER) & 0x3) == 0x1); //assert PC0 is in output mode (01)
+
+  assert(((GPIOB->OTYPER >> 11) & 0x1) == 0x1); //assert PB11 open-drain mode(1)
+  assert(((GPIOB->OTYPER >> 13) & 0x1) == 0x1); //assert PB13 open-drain mode(1)
+  assert(((GPIOB->OTYPER >> 14) & 0x1) == 0x0); //assert PB14 push-pull mode(1)
+  assert(((GPIOB->OTYPER >> 13) & 0x1) == 0x1); //assert PC0 push-pull mode(1) 
+
+  assert(((GPIOB->PUPDR >> (13*2)) & 0x3) == 0x0); //assert PB13 is in no pull (00)
+  assert(((GPIOB->PUPDR >> (11*2)) & 0x3) == 0x0); //assert PB11 is in no pull (00)
+  assert(((GPIOB->AFR[1] >> 12) & 0xF) == 0x1); //assert PB11 is in AF1 mode
+  assert(((GPIOB->AFR[1] >> 20) & 0xF) == 0x5); //assert PB13 is in AF5 mode
+
+  assert(((GPIOB->ODR >> 14) & 0x1) == 0x1); //assert PB14 is high
+  assert(((GPIOC->ODR) & 0x1) == 0x1); //assert PC0 is high
+
+  I2Cx_Setup(I2C2);
+  I2C2->CR1 |= 0x1; // Enable peripheral
+
+  Init_I2C_Transaction(I2C2, 0, 0x69, 0x1); //Initialize Write Transmit
+  writeOp(0x0F); // Write WHO_AM_I Address
+  Init_I2C_Transaction(I2C2, 1, 0x69, 0x1); //Initialize Read Transmit
+
+  uint8_t receivedByte;
+  if (readOp(&receivedByte) == I2C_PASS) { // Attempt to Read
+    if (receivedByte == 0xD4) {
+      HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);
+    } else {HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);}
+  }
+  I2C2->CR2 |= (1 << 14); // Set STOP
+
+  char msg[] = "Test USART\r\n";
 
   while (1)
   {
- 
+    HAL_Delay(1000);
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);
+    sendString(msg);
   }
   return -1;
+}
+
+void sendChar(char c) {
+  while(((USART3->ISR >> 7) & 0x1) == 0x0) {} //wait for tx reg to be empty
+  USART3->TDR = c;
+}
+
+void sendString(const char *str) {
+  while(*str != 0) {
+    sendChar(*str);
+    str++;
+  }
+}
+
+I2C_Status writeOp(uint8_t Byte) {
+  // Wait for TXIS or NACKF Flags set
+  while((((I2C2->ISR >> 1) & 0x1) == 0x0) || (((I2C2->ISR >> 4) & 0x1) == 0x0)) {} 
+  if (((I2C2->ISR >> 1) & 0x1) == 0x1) {
+    I2C2->TXDR = Byte;
+    while (((I2C2->ISR >> 6) & 0x1) == 0); // Wait for TC flag to be set
+    char buffer[64];
+    sprintf(buffer, "Successfully Sent Byte: %u\r\n", Byte);
+    sendString(buffer);
+    return I2C_PASS;
+  }
+  else {
+    return I2C_NACK;
+  }
+}
+
+I2C_Status readOp(uint8_t *Byte) {
+  // Wait for RXNE or NACKF Flags set
+  while((((I2C2->ISR >> 2) & 0x1) == 0x0) || (((I2C2->ISR >> 4) & 0x1) == 0x0)) {} 
+  if (((I2C2->ISR >> 2) & 0x1) == 0x1) {
+    *Byte = I2C2->RXDR;
+    while (((I2C2->ISR >> 6) & 0x1) == 0); // Wait for TC flag to be set
+    char buffer[64];
+    sprintf(buffer, "Successfully Received Byte: %s\r\n", Byte);
+    sendString(buffer);
+    return I2C_PASS;
+  }
+  else {
+    return I2C_NACK;
+  }
+}
+
+void NACKF_Error(void) {
+  char string[] = "NACKF flag set!\r\n";
+  sendString(string);
+  I2C2->ICR |= (1<<4);
+}
+
+void USART3_4_IRQHandler() {
+  receiveReg = USART3->RDR;
+  newData = 1;
 }
 
 /**
