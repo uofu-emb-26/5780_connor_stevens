c4d483a0c65845ca4e4faa9bd9fa49d53d3c6065
ConnorStevens <conngstevens@gmail.com>
2026-03-12 17:20:46 -0700

    setup complete and validated (calibration and enable not tested but implemented)

--- lab6/Src/lab6.c
+++ lab6/Src/lab6.c
@@ -1,5 +1,14 @@
-#include "main.h"
-#include "stm32f0xx_hal.h"
+  #include "main.h"
+  #include "stm32f072xb.h"
+  #include "stm32f0xx_hal.h"
+  #include "stm32f0xx_hal_gpio.h"
+  #include <assert.h>
+  #include <stdio.h>
+  #include "hal_gpio.h"
+  #include "core_cm0.h"
+  #include "stm32f0xx_hal_gpio_ex.h"
+  #include "stm32f0xx_hal_rcc.h"
+  #include <stdlib.h>
 
 void SystemClock_Config(void);
 
@@ -13,10 +22,45 @@ int main(void)
   HAL_Init();
   /* Configure the system clock */
   SystemClock_Config();
+  HAL_RCC_GPIOC_CLK_ENABLE();
+  RCC_ADC_CLK_ENABLE();
+
+  //Setup ALL LEDs and PC0
+  GPIO_InitTypeDef iniStr1 = {GPIO_PIN_8 | GPIO_PIN_7 |GPIO_PIN_6 | GPIO_PIN_9 | GPIO_PIN_0,
+                            GPIO_MODE_OUTPUT_PP,
+                            GPIO_NOPULL,
+                            GPIO_SPEED_FREQ_LOW};
+  My_HAL_GPIOx_Init(GPIOC, &iniStr1);
+  GPIOC->MODER |= 0x3; // Set PC0 to analog mode (11)
+
+  assert(((GPIOC->MODER >> (6*2)) & 0x3) == 0x1); //assert PC6 is in output mode (01)
+  assert(((GPIOC->MODER >> (7*2)) & 0x3) == 0x1); //assert PC7 is in output mode (01)
+  assert(((GPIOC->MODER >> (8*2)) & 0x3) == 0x1); //assert PC8 is in output mode (01)
+  assert(((GPIOC->MODER >> (9*2)) & 0x3) == 0x1); //assert PC9 is in output mode (01)
+  assert(((GPIOC->MODER) & 0x3) == 0x3); //assert PC0 is in analog mode (11)
+  assert(((GPIOC->PUPDR) & 0x3) == 0x0); //assert PC0 is in no pull/push (00)
+
+
+  ADC_Configuration();
+  ADC1->CHSELR |= (1 << 10); // set ADC channel to 10
+
+  assert(((ADC1->CFGR1 >> 13) & 0x1) == 0x1); // check continuous conversion mode
+  assert(((ADC1->CFGR1 >> 10) & 0x3) == 0x0); // check hardware trigger disabled (00)
+  assert(((ADC1->CFGR1 >> 3) & 0x3) == 0x2); // check 8 bit resolution (10)
+  assert(((ADC1->CHSELR >> 10) & 0x1) == 0x1); // check channel 10 is enabled
+
+  ADC1->CR |= (1 << 31); // Start calibration process
+  while (((ADC1->CR >> 31) & 0x1) == 0x0) {} // Wait for calibration process to complete
+
+  ADC1->CR |= 0x1; // Enable ADC
+  while (((ADC1->ISR) & 0x1) == 0x0) {} // Wait for ADC to enable
+
+  ADC1->CR |= (1 << 2); // Start ADC conversion
 
   while (1)
   {
- 
+    HAL_Delay(300);
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);
   }
   return -1;
 }
